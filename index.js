b37f073e15759ee8338968e22129f4ce1835cee2
Deepinder Singh <deepindersingh@Deepinders-MacBook-Air.local>
2021-01-25 15:44:15 +0530

    some deep concepts of javascript

---
+++ index.js
@@ -0,0 +1,24 @@
+// console.log('hello'); 
+
+// we can do it in console also
+
+
+
+
+
+
+
+
+
+/** 24 min values and veriables in javascript */
+
+// you cannot use reserved keyword for javascript var names
+var myName = 'Deepinder Singh';
+var myAge = 21;
+
+console.log(myName + myAge);
+
+// var _myName = 'vinod';
+// var _1my__Name = 'bahadur';
+// var 1myName = 'thapa'
+// var $myName = 'thapa technical';
